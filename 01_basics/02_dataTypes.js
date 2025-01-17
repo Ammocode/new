e30029eca80d8c0f008998925ad4198fed0ec293
Ammocode <amogha024@gmail.com>
2025-01-17 17:43:58 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -4,3 +4,23 @@
 
 console.log(3
     +3); //code readability should be high
+
+    let name="amogha"
+    let age=19
+    let isLoggedIn=false
+    let state;
+
+    //number=> 2 to the power 53
+    //bigint
+    // string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefines => 
+    //symbol => unique
+
+    //object 
+
+    console.log(typeof "amogha")
+    console.log(typeof null) //object
+    console.log(typeof undefined) //undefined
+
